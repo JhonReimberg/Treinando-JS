df1c132df746b8c22f6e47c23e859cf7eff3cb60
joao reimbss <joaoreimberg09@gmail.com>
2021-03-30 19:00:47 -0300

    outros testes

---
+++ if ou else.js
@@ -0,0 +1,12 @@
+let correr = 18
+
+if(correr == 40) {
+console.log("começou a cansar")
+}
+
+else if (correr <=39 && correr >=1 ){
+    console.log("ta suave ainda")
+}else {
+
+console.log("cansado")
+}
---
+++ switch.js
@@ -0,0 +1,19 @@
+let expressao = 'a'
+
+switch (expressao) {
+    
+case 'a':
+    //código
+    console.log('a')
+    break
+    //usando o break vc para nessa linha de código, no caso = a
+
+case 'b':
+//código
+console.log('b')
+break
+
+default:
+    console.log('default')
+    break
+}
