5e8f352d3a9e56dc3d892393ac2d0d0c82133400
joao reimbss <joaoreimberg09@gmail.com>
2021-04-02 15:41:19 -0300

    a

---
+++ teste final.js
@@ -0,0 +1,51 @@
+/*   90 a 100 = A
+     80 a 99 = B
+     79 a 70 = C
+     69 a 60 = D 
+     59 a 0 = F
+
+
+
+let notas = 61
+
+if(notas >= 90 ) {
+console.log("A")
+}
+
+else if (notas <= 89 && notas >= 80 ){
+
+    console.log("B")
+}
+
+else if (notas <= 79 && notas >=70 ){
+
+    console.log("C")
+}
+    else if (notas <= 69 && notas >= 60 ){
+
+        console.log("D")
+    }    else {
+
+        console.log("F")
+    }
+    */
+
+
+   /* let dinheiro = {
+
+        incomes: [2500,1000,100,400],
+        expenses: [200,300,400,1200]
+    }
+
+    function calculo(){
+
+        
+    }
+    )
+/*
+
+    
+
+
+
+    
